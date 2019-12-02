9091775fea54c28edd259a88c9e8906598c7bd88
Lyhne1997 <55769759+Lyhne1997@users.noreply.github.com>
2019-12-02 12:53:17 +0100

    123

--- BananaSplit/GameObject.cs
+++ BananaSplit/GameObject.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,23 @@ using System.Threading.Tasks;
 
 namespace BananaSplit
 {
-    class GameObject
+    public abstract class GameObject
     {
+        //Flere sprites i et array.
+        protected Texture2D[] sprites;
+        //En sprite.
+        protected Texture2D sprite;
+        //Frames per seconds, bruges til animation af sprites.
+        protected int fps;
+        //Bruges til Player og Enemey movement.
+        protected float speed;
+        //Bruges også til Player og Enemy movement.
+        protected float velocity;
+        //Bruges til Player og Enemy position.
+        protected Vector2 origin;
+        //Holder styr på hvilken tast som bliver brugt i øjeblikket.
+        protected KeyboardState currentKey;
+        //Holder styr på hvilken key som blev brugt sidst.
+        protected KeyboardState previousKey;
     }
 }
--- BananaSplit/Player.cs
+++ BananaSplit/Player.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,27 @@ using System.Threading.Tasks;
 
 namespace BananaSplit
 {
-    class Player
+    class Player : GameObject
     {
+
+        
+
+
+       public void LoadContent(ContentManager content)
+        {
+            sprites = new Texture2D[3];
+
+            for (int i = 0; i < sprites.Length; i++)
+            {
+                sprites[i] = content.Load<Texture2D>(i + 1 + ("playersprite"));
+            }
+            fps = 5;
+
+            
+        }
+        public void HandleInput(GameTime gameTime)
+        {
+
+        }
     }
 }
